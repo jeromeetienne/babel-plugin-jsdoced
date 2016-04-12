88629ba3ad736605efdbd0db489f69fcd0750919
Jerome Etienne <jerome.etienne@gmail.com>
2016-04-12 14:38:46 +0100

    more work

--- examples/browser-sourcemaps-build.js
+++ examples/browser-sourcemaps-build.js
@@ -1,25 +1,23 @@
 /**
  * This is a super function
  * @param {String} myString1 - a super string
- * @param {String|Number|Function|THREE.Vector3} myString2 - a super string
- * @return {Number|String} - super returned string
+ * @param {String} myString2 - a super string
+ * @return {String} - super returned string
  */
 function myFunctionDeclaration(myString1, myString2) {
   {
     console.assert(typeof myString1 === 'string', "Invalid type for Params 0 myString1");
-    console.assert(typeof myString2 === 'string' || typeof myString2 === 'number' || myString2 instanceof Function || myString2 instanceof THREE.Vector3, "Invalid type for Params 1 myString2");
+    console.assert(typeof myString2 === 'string', "Invalid type for Params 1 myString2");
   }
-
-  console.log('now is', new Date());
-  if (true) {
-      var _returnValue = 'food';
-      console.assert(typeof _returnValue === 'number' || typeof _returnValue === 'string', "Invalid type for return value");
-      return _returnValue;
-    }
   {
-    var _returnValue2 = myString1 + myString1 + '---' + myString2;
+    var _returnValue = 3;
+    console.assert(typeof _returnValue === 'string', "Invalid type for return value");
+    return _returnValue;
+  }
+  {
+    var _returnValue2 = myString1 + myString2;
 
-    console.assert(typeof _returnValue2 === 'number' || typeof _returnValue2 === 'string', "Invalid type for return value");
+    console.assert(typeof _returnValue2 === 'string', "Invalid type for return value");
     return _returnValue2;
   }
 }
--- examples/browser-sourcemaps.js
+++ examples/browser-sourcemaps.js
@@ -1,14 +1,13 @@
 /**
  * This is a super function
  * @param {String} myString1 - a super string
- * @param {String|Number|Function|THREE.Vector3} myString2 - a super string
- * @return {Number|String} - super returned string
+ * @param {String} myString2 - a super string
+ * @return {String} - super returned string
  */
 function myFunctionDeclaration(myString1, myString2){
-        console.log('now is', new Date())
-        if( true ) return 'food'
-        return myString1+myString1+'---'+myString2;
+        return myString1+myString2;
 }
 
 
 console.log('return is ', myFunctionDeclaration('foo', 'bar'))
+ 
