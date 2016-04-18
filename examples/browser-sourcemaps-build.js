9b068b36b785b8ef34cf4e9450db128af5ce4273
Jerome Etienne <jerome.etienne@gmail.com>
2016-04-18 17:17:55 +0100

    update sourceMap

--- examples/browser-sourcemaps-build.js
+++ examples/browser-sourcemaps-build.js
@@ -5,16 +5,15 @@
  * @return {String} - super returned string
  */
 function myFunctionDeclaration(myString1, myString2) {
-  {
-    console.assert(typeof myString1 === 'string', "Invalid type for Params 0 myString1");
-    console.assert(typeof myString2 === 'string', "Invalid type for Params 1 myString2");
-  }
-  {
-    var _returnValue = myString1 + myString2;
+  console.assert(typeof myString1 === 'string', "Invalid type for argument 0 myString1");
+  console.assert(typeof myString2 === 'string', "Invalid type for argument 1 myString2");
 
-    console.assert(typeof _returnValue === 'string', "Invalid type for return value");
-    return _returnValue;
-  }
+  console.log('hello');
+
+  var _returnValue = myString1 + myString2;
+
+  console.assert(typeof _returnValue === 'string', "Invalid type for return value");
+  return _returnValue;
 }
 
 console.log('return is ', myFunctionDeclaration('foo', 'bar'));
