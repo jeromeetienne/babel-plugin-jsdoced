6b0f963ce4f6f28e8c3432c0cffbc3d48e0724c8
Jerome Etienne <jerome.etienne@gmail.com>
2016-04-18 17:14:34 +0100

    more work

--- examples/testbed.js
+++ examples/testbed.js
@@ -1,16 +1,7 @@
 /**
-<<<<<<< HEAD
- * blabla
- * @return {Number} - blsbls
- */
-function Point() {
-	return
-}
-=======
 * A description
 * @param {String} num1 - The first number.
 */
 function foo(num1) {
         // ...
 }
->>>>>>> fcbc91de6bfb2ef62aa279c1433fc14858dfc288
